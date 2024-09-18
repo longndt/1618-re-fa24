76607d14e25938f395417858daf55ed7ff3e7ddd
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-09-18 10:12:57 +0700

    lesson1

---
+++ Lesson01/Example01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello world");
+Console.WriteLine("Programming - 1618");
+
+/*
+ * Write: print to screen 
+ * WriteLine: print to screen and move to new line
+ */
+
+//How to run ?
+//1st way: Click "Run" (Play) button
+//2nd way: CTRL + F5
+
+// single line comment 
+/*
+ * multiple
+ * lines
+ * comment
+ */
